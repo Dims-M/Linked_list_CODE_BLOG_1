d5f6c3fc516144233c622883141a0e59f3027caa
Dims_M <dimsxm@yandex.ru>
2019-05-10 21:40:58 +0300

    Добавьте файлы проекта.

---
+++ Linked_list_CODE_BLOG/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_list_CODE_BLOG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
